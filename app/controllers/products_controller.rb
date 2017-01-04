52e89aa0f7a8afc19194cae56fc2b2182a07bc7d
danieltbrad <danieltbrad@gmail.com>
2017-01-04 07:35:57 -0800

    Add column sort with direction toggling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,18 @@
 class ProductsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @products = Product.all
+    @products = Product.order(sort_column + " " + sort_direction)
   end
+
+  private
+
+  def sort_column
+    Product.column_names.include?(params[:sort]) ? params[:sort] : "model"
+  end
+
+  def sort_direction
+    ["asc", "desc"].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
 end
